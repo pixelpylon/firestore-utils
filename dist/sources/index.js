ddb6da679d5ee64b8e4986df93fba327c524cf7e
Ivan Kosolapov <polzuka@gmail.com>
2023-08-28 09:29:10 +0500

    EntityManager

--- dist/sources/index.js
+++ dist/sources/index.js
@@ -2,10 +2,12 @@ const {Repository} = require('./Repository')
 const {Transaction} = require('./Transaction')
 const DocumentAccessor = require('./DocumentAccessor')
 const {StatusManager} = require('./StatusManager')
+const {EntityManager} = require('./EntityManager')
 
 module.exports = {
   Repository,
   Transaction,
   DocumentAccessor,
   StatusManager,
+  EntityManager,
 }
